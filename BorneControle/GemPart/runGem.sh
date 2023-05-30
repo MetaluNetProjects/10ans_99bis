d757aef843fbbfb2763f3718b8d6d3a65827773a
Antoine Rousseau <_antoine_@metalu.net>
2023-05-30 11:21:38 +0200

    updates and bugfixes on Borne

--- BorneControle/GemPart/runGem.sh
+++ BorneControle/GemPart/runGem.sh
@@ -3,7 +3,9 @@
 #xrandr -d :0 --output DVI-I-1 --panning 1366x768+0+0
 #xrandr -d :0 --output DP-0 --panning 1366x768+1366+0
 
+if [ $DISPLAY = ":0" ];then FLAG="-stderr"; fi
+
 export DISPLAY=$1
 
-pd -lib Gem -noaudio -nrt -open $(dirname $0)/0Gem.pd
+pd -lib Gem $FLAG -noaudio -nrt -open $(dirname $0)/0Gem.pd
 
