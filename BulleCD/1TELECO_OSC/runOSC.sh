2904d31e3d61ac044cb5c9d7fa42067904035396
Antoine Rousseau <_antoine_@metalu.net>
2023-06-02 10:49:27 +0000

    adapt runOSC.sh for rPi (use node version)

--- BulleCD/1TELECO_OSC/runOSC.sh
+++ BulleCD/1TELECO_OSC/runOSC.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 cd `dirname $0`
-open-stage-control  -s localhost:18080 -t ./theme-contrast.css -l ./teleco-OSC.json
+O_S_C=open-stage-control
+if [ ! $(which open-stage-control) ]; then 
+	O_S_C="node /mnt/mmcblk0p2/App/open-stage-control_1.25.0_node"
+	fi
+
+#node /mnt/mmcblk0p2/App/open-stage-control_1.25.0_node -s localhost:18080 -t ./theme-contrast.css -l ./teleco-OSC.json
+${O_S_C} -s localhost:18080 -t ./theme-contrast.css -l ./teleco-OSC.json
 # --read-only
+
